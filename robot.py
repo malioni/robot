c20d5dff86bfac134d671bbef0135e90d353dbdc
Matiss Senkans <matisso95@gmail.com>
2019-02-25 19:02:01 -0700

    Initial robot commit

---
+++ robot.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return """                                                                                       
+    <html>                                                                                           
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>         
+    <script>                                                                                         
+      function forward() { $.ajax({url: "/forward"}) }                                               
+    </script>                                                                                        
+    <button onclick="forward()">Forward</button>                                                     
+    </html>                                                                                          
+    """
+
+@app.route("/forward")
+def forward():
+    print("Robo go forward!")
+    return jsonify({
+        "message": "Robot just went forward"
+    })
+
